9de4c19d757a9036cf2c26dfcf6d768ad4373608
16099 <16099@wc.school.nz>
2020-07-28 10:15:56 +1200

    starting coding

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
+def render_homepage():
     return render_template('home.html')
+
+
+app.run(host='0.0.0.0', debug=True)
+
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+
 app.run(host='0.0.0.0', debug=True)
