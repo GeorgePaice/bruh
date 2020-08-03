a247b194fa189cf18485239824bf474d6a63300b
16099 <16099@wc.school.nz>
2020-08-04 10:14:55 +1200

    messing around

--- app.py
+++ app.py
@@ -12,8 +12,47 @@ app.run(host='0.0.0.0', debug=True)
 
 
 @app.route('/shop')
-def shop():
+def render_shop_page():
     return render_template('shop.html')
 
 
 app.run(host='0.0.0.0', debug=True)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def render_login_page():
+    if is_logged_in():
+        return redirect('/')
+
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+
+        query = """SELECT id, fname, password FROM customer WHERE email = ?"""
+        con = create_connection(DB_NAME)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        # if given the email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty resultset
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+
+        # check if the password is incorrect for that email address
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        print(session)
+        return redirect('/')
+
+    return render_template('login.html', logged_in=is_logged_in())
+
