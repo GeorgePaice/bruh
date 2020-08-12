bff9366e0a78ff336b4fb30f274b472a9819c63b
16099 <16099@wc.school.nz>
2020-08-12 12:30:12 +1200

    nav

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ def render_homepage():
     return render_template('home.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/shop')
 def shop():
     return render_template('shop.html')
