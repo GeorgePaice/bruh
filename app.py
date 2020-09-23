c38f1935c4168a12bb398dd4474c4500b563c101
16099 <16099@wc.school.nz>
2020-09-23 12:29:55 +1200

    comit

--- app.py
+++ app.py
@@ -24,9 +24,42 @@ def render_homepage():
     return render_template('home.html')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/login', methods=["GET", "POST"])
+def render_login_page():
+    if is_logged_in():
+        return redirect('/')
+
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+
+        query = """SELECT id, fname, password FROM customer WHERE email = ?"""
+        con = create_connection(DB_NAME)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        # if given the email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty resultset
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+
+        # check if the password is incorrect for that email address
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        print(session)
+        return redirect('/')
+
+    return render_template('login.html', logged_in=is_logged_in())
 
 
 @app.route('/fans')
@@ -56,7 +89,7 @@ def switches():
     con = create_connection(DB_NAME)
 
     # SELECT the things you want from your table(s)
-    query = "SELECT id, name, description, quantity, image, price FROM switches"
+    query = "SELECT id, name, description, quantity, image, price, link FROM switches"
 
     cur = con.cursor()  # You need this line next
     cur.execute(query)  # this line actually executes the query
@@ -73,6 +106,45 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def render_signup_page():
+    if is_logged_in():
+        return redirect('/')
+
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            return redirect('/signup?error=Passwords+dont+match')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+8+characters+or+more')
+
+        hashed_password = bcrypt.generate_password_hash(password)
+
+        con = create_connection(DB_NAME)
+
+        query = "INSERT INTO customer(id, fname, lname, email, password) " \
+                "VALUES(NULL,?,?,?,?)"
+
+        cur = con.cursor()  # You need this line next
+        try:
+            cur.execute(query, (fname, lname, email, hashed_password))  # this line actually executes the query
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=Email+is+already+used')
+
+        con.commit()
+        con.close()
+        return redirect('/login')
+
+    return render_template('signup.html', logged_in=is_logged_in())
+
 
 
 app.run(host='0.0.0.0', debug=True)
